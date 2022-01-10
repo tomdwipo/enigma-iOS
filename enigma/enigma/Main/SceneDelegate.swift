37e070fe2551be287ea1201a21cb6688e1c1d257
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2022-01-10 16:55:48 +0700

    c

--- enigma/enigma/Main/SceneDelegate.swift
+++ enigma/enigma/Main/SceneDelegate.swift
@@ -13,8 +13,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+      
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        window.rootViewController = TestViewController()
+//        window.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
         self.window = window
         self.window?.makeKeyAndVisible()
     }
--- enigma/enigma/Test/TestViewController.swift
+++ enigma/enigma/Test/TestViewController.swift
@@ -8,14 +8,24 @@
 import UIKit
 
 class TestViewController: UIViewController {
-
+    @IBOutlet weak var testText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.testing(_:)))
+
+        testText.addGestureRecognizer(tap)
+        testText.isUserInteractionEnabled = true
 print("oke")
         // Do any additional setup after loading the view.
     }
 
+    
+    @objc func testing(_ sender: UITapGestureRecognizer){
+        print("grsd")
+        let vc =  UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        self.present(vc!, animated: true)
+    }
 
     /*
     // MARK: - Navigation
